d38e343b32f7b603446f86211c76127d9bb984fb
mille4kg <mille4kg@dukes.jmu.edu>
2020-02-10 12:48:28 -0500

    Changed to one line statements

--- main.cpp
+++ main.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
 using namespace std;
+
 int main() {
   for (int i = 0; i <= 100; i++) {
-    if(i % 15 == 0){
-      cout << "Fizzbuzz" << endl;
-    } else if(i % 5 == 0){
-      cout << "Buzz" << endl;
-    } else if(i % 3 == 0) {
-      cout << "Fizz" << endl;
-    } else {
-      cout << i << endl;
-    }
+    if(i % 15 == 0) cout << "Fizzbuzz" << endl;
+    else if(i % 5 == 0) cout << "Buzz" << endl;
+    else if(i % 3 == 0) cout << "Fizz" << endl;
+    else cout << i << endl;
   }
 } 
